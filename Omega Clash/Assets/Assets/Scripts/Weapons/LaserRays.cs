751bf7b9302393143390a3319ff6457cb5467f30
Ruben Aguiar <r.aguiar9080@gmail.com>
2013-06-03 12:23:11 +0100

    Mirrors Working

--- Omega Clash/Assets/Assets/Scripts/Weapons/LaserRays.cs
+++ Omega Clash/Assets/Assets/Scripts/Weapons/LaserRays.cs
@@ -1,40 +1,74 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LaserRays : MonoBehaviour {
 	
 	public Transform LaserRay;
 	
-	private Transform before;
+
+	public List<Transform> before = new List<Transform>();
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		if(before!=null)
-			Destroy(before.gameObject);
+		if(before.Count!=0)
+		{
+			for(int i=0;i<before.Count;i++)
+			{
+				Destroy(before[i].gameObject);
+			}
+			before.Clear();
+		}
 		if(Input.GetMouseButton(0))
 		{
-			Vector3 fwd = transform.TransformDirection(Vector3.forward);
-			RaycastHit info = new RaycastHit();
-			//Debug.DrawRay(transform.position,fwd,Color.red,1000);
-        	if (Physics.Raycast(transform.position, fwd, out info, 1000.0f))
+			Transform origin = new GameObject().transform;
+			origin.position = transform.position;
+			origin.forward = transform.forward;
+			int count=0;
+			while(true && count<3)
 			{
-				Vector3 halfway = transform.position + ((info.point - transform.position)/2);
-				float halfdistance = ((info.point - transform.position).magnitude)/2;
-				before = (Transform)Network.Instantiate(LaserRay, halfway, transform.rotation, 0);
-				before.localScale += new Vector3(0,halfdistance,0);
-				before.Rotate(new Vector3(90,0,0));
-
-				if(info.collider.gameObject.tag == "Mirror")
+				count++;
+				Vector3 fwd = origin.TransformDirection(Vector3.forward);
+				RaycastHit info = new RaycastHit();
+				//Debug.DrawRay(transform.position,fwd,Color.red,1000);
+	        	if (Physics.Raycast(origin.position, fwd, out info, 1000.0f))
 				{
-					Vector3 direction = info.point - transform.position;
-					Vector3 reflection = Vector3.Reflect(direction, info.normal);
-
-					Debug.DrawRay(info.point,reflection,Color.red,1000);
-					//print("MIRROR" + Random.Range(-100,100));
+					Vector3 halfway = origin.position + ((info.point - origin.position)/2);
+					float halfdistance = ((info.point - origin.position).magnitude)/2;
+					Transform myray;
+					myray=(Transform)Network.Instantiate(LaserRay, halfway, origin.rotation, 0);
+					myray.localScale += new Vector3(0,halfdistance,0);
+					myray.Rotate(new Vector3(90,0,0));
+					
+					before.Add(myray);
+	
+					if(info.collider.gameObject.tag == "Mirror")
+					{
+						Vector3 direction = info.point - origin.position;
+						Vector3 reflection = Vector3.Reflect(direction, info.normal);
+	
+						origin.position = info.point;
+						origin.forward = reflection;
+						Debug.DrawRay(origin.position,origin.forward,Color.red,2);
+						//print("MIRROR" + Random.Range(-100,100));
+					}
+					else
+					{						
+						return;
+					}
+				}
+				else
+				{
+					Vector3 halfway = origin.position + (fwd * 500);
+					Transform myray;
+					myray=(Transform)Network.Instantiate(LaserRay, halfway, origin.rotation, 0);
+					myray.localScale += new Vector3(0,500,0);
+					myray.Rotate(new Vector3(90,0,0));
+					
+					before.Add(myray);
 				}
 			}
-
 		}
 	}
 }
